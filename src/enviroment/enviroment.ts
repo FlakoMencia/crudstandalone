1f0d01a5e21d885a47420b9c073b211c057c85d9
Flako <flakomencia@gmail.com>
2023-03-22 00:25:42 -0600

    baby steps

---
+++ src/enviroment/enviroment.ts
@@ -0,0 +1,11 @@
+export const enviroment = {
+     firebaseConfig : {
+        apiKey: "AIzaSyAgI1l5doYLqK9ix30HBp-ccmeywWaRgyQ",
+        authDomain: "flakoprueba.firebaseapp.com",
+        projectId: "flakoprueba",
+        storageBucket: "flakoprueba.appspot.com",
+        messagingSenderId: "130083324684",
+        appId: "1:130083324684:web:a35184ae96358f53d67f29"
+      }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideFirebaseApp , initializeApp } from '@angular/fire/app';
+import { getFirestore , provideFirestore } from '@angular/fire/firestore';
+import { enviroment } from './enviroment/enviroment';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(
+      provideFirebaseApp( () => initializeApp (enviroment.firebaseConfig)),
+      provideFirestore( () => getFirestore())
+      ),
+  ]
+}).catch((err) => console.log(err));
