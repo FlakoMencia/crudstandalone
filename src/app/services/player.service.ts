1bf1d7c3f3d71f4bc274ec543be7e8d8cd0a7adc
Flako <flakomencia@gmail.com>
2023-05-17 22:21:42 -0600

    for update

---
+++ src/app/services/player.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, Firestore, query, where } from '@angular/fire/firestore';
+import { Player } from '../commons/interfaces/player.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+
+  constructor(private firestore : Firestore) { }
+
+  addPlayer(player: Player){
+    const playerRef = collection(this.firestore, 'players');
+    return addDoc(playerRef, player);
+  }
+
+  getPlayer(filter=''){
+    const playerRef = collection(this.firestore, 'players');
+    let q = query(playerRef);
+    if(filter){
+      q = query(playerRef, where('name','==', filter));
+    }
+    return collectionData(q);
+
+  }
+
+}
